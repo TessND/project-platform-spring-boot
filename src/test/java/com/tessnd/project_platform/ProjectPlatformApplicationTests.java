b300dc7bf3bfece50bc5382cd20f853b6502b7cc
TessND <114303080+TessND@users.noreply.github.com>
2024-12-19 19:19:27 +0400

    first commit

---
+++ src/test/java/com/tessnd/project_platform/ProjectPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tessnd.project_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
